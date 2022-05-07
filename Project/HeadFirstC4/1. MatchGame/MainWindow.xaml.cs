bb65610a3c008b23b114c71e88f9fa932ca283d3
Harinder Dulai <harinder.dulai@wpnc.agency>
2022-05-07 12:08:38 +0100

    inital commit

--- Project/HeadFirstC4/1. MatchGame/MainWindow.xaml.cs
+++ Project/HeadFirstC4/1. MatchGame/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace _1._MatchGame
 
         private void SetUpGame()
         {
+            //Create a list of 8 pairs of emojis
             List<string> animalEmoji = new List<string>()
             {
                 "🦧", "🦧",
@@ -40,14 +41,46 @@ namespace _1._MatchGame
                 "🦙",  "🦙"
             };
 
+            //Create a new random number generator
             Random random = new Random();
 
+            //Find every TextBlock in the main grid and repeate the following statement for each of them
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
+                //Pick a random number between 0 and the number of emojis left in the list and call it index
                 int index = random.Next(animalEmoji.Count);
+
+                //Use the random number called index to get a random emoji from the list
                 string nextEmoji = animalEmoji[index];
+                //Update the text block with the random emoji from the list
                 textBlock.Text = nextEmoji;
+                //Remove the random emoji from the list
                 animalEmoji.RemoveAt(index);
+            }
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock; 
+
+            if(findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
             }
         }
     }
